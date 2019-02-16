92fe73dc0bfdd4f043f17c0de23d624d44b3b745
Asia <joannawytrzes@gmail.com>
2019-02-16 15:13:05 +0100

    Display photos from API

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,9 @@
         </transition>
         <Claim v-if="step === 0"/>
         <SearchInput v-model="searchValue" @input="handleInput" :dark="step === 1"/>
+        <div class="results" v-if="results && !loading && step === 1">
+            <Item v-for="item in results" :item="item" :key="item.data[0].nasa_id"/>
+        </div>
     </div>
 </template>
 
@@ -17,6 +20,7 @@ import debounce from 'lodash.debounce';
 import Claim from '@/components/Claim.vue';
 import SearchInput from '@/components/SearchInput.vue';
 import HeroImage from '@/components/HeroImage.vue';
+import Item from '@/components/Item.vue';
 
 const API ='https://images-api.nasa.gov/search';
 
@@ -26,6 +30,7 @@ export default {
         Claim,
         SearchInput,
         HeroImage,
+        Item
     },
     data() {
         return {
